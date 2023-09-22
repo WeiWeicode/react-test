5894ebc56166be83216786300736e7a80e08e118
S112009 <S112009@S1800-S112009>
2023-09-22 17:03:40 +0800

    練習

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Button, ButtonGroup, Checkbox, CheckboxGroup, Stack, HStack, VStack } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
@@ -9,9 +10,28 @@ function App() {
   return (
     <>
 
-    {/* title:首頁 */}
-    <h1>首頁</h1>
-   
+      {/* title:首頁 */}
+      <h1>首頁</h1>
+      {/* <Button colorScheme='blue'>Button</Button> */}
+      {/* test按鈕網頁跳轉到/test */}
+
+      <Stack spacing={[1, 5]} direction={['column', 'row']}>
+        <Button  colorScheme='blue' onClick={() => window.location.href = '/test'}>跳轉到Test</Button>
+        {/* 首頁按鈕到/ */}
+        <Button colorScheme='blue' onClick={() => window.location.href = '/'}>跳轉到首頁</Button>
+      </Stack>
+      <Stack spacing={[1, 5]} direction={['column', 'row']}>
+        <Checkbox size='sm' colorScheme='red'>
+          Checkbox
+        </Checkbox>
+        <Checkbox size='md' colorScheme='green' defaultChecked>
+          Checkbox
+        </Checkbox>
+        <Checkbox size='lg' colorScheme='orange' defaultChecked>
+          Checkbox
+        </Checkbox>
+      </Stack>
+
 
     </>
   )
--- vite-react/src/main.jsx
+++ vite-react/src/main.jsx
@@ -8,11 +8,12 @@ import {
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import Test from './components/test.jsx';
+import { ChakraProvider } from '@chakra-ui/react'
 import './index.css';
 
 const router = createBrowserRouter([
   {
-    path: "/",    
+    path: "/",
   },
   {
     path: "/test",
@@ -20,9 +21,15 @@ const router = createBrowserRouter([
   },
 ]);
 
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      {/* <TheRestOfYourApplication /> */}
+      <App />
+    </ChakraProvider>
+
     <RouterProvider router={router} />
   </React.StrictMode>
 );
